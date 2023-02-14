0347cbd63806272d0cf950365a7eef8ed9c871bc
Vanessa Park <bohyunparkv@gmail.com>
2023-02-14 23:53:18 +0900

    Implementing React Query to imporve on rerendering issue using custom hooks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import MainProducts from "./components/MainProducts";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />
+    </QueryClientProvider>
+  );
+}
---
+++ src/components/Products.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query;";
+
+export default function Products() {
+  const [checked, setChecked] = useState(false);
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery(["products"], async () => {
+    console.log("fetching");
+    return fetch(`data/products.json`).then((res) => res.json());
+  });
+  // const [loading, error, products] = useProducts({ salesOnly: checked });
+  const handleChange = () => setChecked((prev) => !prev);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <>
+      <label>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+        Show Only 🔥 Sale
+      </label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}

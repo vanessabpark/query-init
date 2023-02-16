8ba0e18a8eb549465a100ddde07fc7514b99db46
Vanessa Park <bohyunparkv@gmail.com>
2023-02-16 23:57:37 +0900

    Error fix: react-query spelling error and adding dependency

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useQuery } from "@tanstack/react-query;";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Products() {
   const [checked, setChecked] = useState(false);

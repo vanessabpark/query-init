de58ad4b863d60fcac0a3964256038b188da73b3
Vanessa Park <bohyunparkv@gmail.com>
2023-02-20 23:14:34 +0900

    Invalidating query

--- src/components/MainProducts.jsx
+++ src/components/MainProducts.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import Products from './Products';
+import React, { useState } from "react";
+import Products from "./Products";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function MainProducts() {
   const [showLeftProducts, setShowLeftProducts] = useState(true);
   const [showRightProducts, setShowRightProducts] = useState(true);
+  const client = useQueryClient();
+
   return (
-    <main className='container'>
+    <main className="container">
       <div>
         {showLeftProducts && <Products />}
         <button onClick={() => setShowLeftProducts((show) => !show)}>
@@ -18,6 +21,13 @@ export default function MainProducts() {
           Toggle
         </button>
       </div>
+      <button
+        onClick={() => {
+          client.invalidateQueries(["products", false]);
+        }}
+      >
+        Updated info
+      </button>
     </main>
   );
 }
